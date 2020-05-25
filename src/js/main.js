ed7f50a5a310e16b40db11d1f56729a3e24c1739
Lareta <lareta064@gmail.com>
2020-05-25 14:31:56 +0300

    версия-1

--- src/js/main.js
+++ src/js/main.js
@@ -198,5 +198,18 @@ $(document).ready(function () {
         }
 
     });
+    /*кнопка вверх */
+    $("#back-top").hide();
+
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+    });
 
 })
